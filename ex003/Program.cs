25e865d67bd098bbee3ec1a73a0b01bf09b60761
said_sadu <sadullaev.1996@gmail.com>
2022-07-25 14:16:26 +0300

    решение 3 задачи

---
+++ ex003/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите число: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube)
+{
+    int pos = 0;
+    int length = cube.Length;
+    while (pos < length)
+    {
+        cube[pos] = Convert.ToInt32(Math.Pow(pos, 3));
+        pos++;
+    }
+}
+
+void PrintArry(int[] coll)
+{
+    int count = coll.Length;
+    int index = 0;
+    while (index < count)
+    {
+        Console.Write(coll[index] + " ");
+        index++;
+    }
+}
+
+int[] arry = new int[cube + 1];
+Cube(arry);
+PrintArry(arry);
